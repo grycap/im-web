90a5c3c9bbe65517899f709427a26fa717851e25
micafer <micafer1@upv.es>
2015-04-02 18:21:14 +0200

    Add GetVMProperty function to im.php

--- im.php
+++ im.php
@@ -184,6 +184,27 @@ function GetVMInfo($host, $port, $inf_id, $vm_id) {
         }
 }
 
+function GetVMProperty($host, $port, $inf_id, $vm_id, $property) {
+	$auth = get_auth_data();
+	$xmlrpc_client = new xmlrpc_client('/',$host,$port);
+	$xmlrpc_msg = new xmlrpcmsg('GetVMProperty', array(new xmlrpcval((int)$inf_id, "int"), new xmlrpcval($vm_id, "string"), new xmlrpcval($property, "string"), $auth));
+
+	$xmlrpc_resp = $xmlrpc_client->send($xmlrpc_msg);
+
+	if ($xmlrpc_resp->faultCode())
+		return 'Error: ' . $xmlrpc_resp->faultString();
+	else
+		$res = php_xmlrpc_decode($xmlrpc_resp->value());
+	$success = $res[0];
+	$info = $res[1];
+
+	if ($success) {
+		return $info;
+	} else {
+		return 'Error';
+	}
+}
+
 function AddResource($host, $port, $inf_id, $radl) {
     $auth = get_auth_data();
     $xmlrpc_client = new xmlrpc_client('/',$host,$port);
