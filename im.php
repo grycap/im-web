1ce5c9d3198278a96dba66b776e09d8966c98f7b
micafer <micafer1@upv.es>
2017-11-14 10:47:22 +0100

    Fix error

--- im.php
+++ im.php
@@ -1,12 +1,13 @@
 <?php
+include_once('im-rest.php');
+include_once('im-xml-rpc.php');	
+
 function GetIM() {
 	include('config.php');
 
 	if ($im_use_rest) {
-		include('im-rest.php');
 		return IMRest::connect($im_host,$im_port);
 	} else {
-		include('im-xml-rpc.php');	
 		if ($im_use_ssl) {
 			$im_method = 'https';
 		} else {
