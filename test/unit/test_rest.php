9cb18e9fc8cefb41a4d0a0554c1feb9f29b1a7ab
micafer <micafer1@upv.es>
2017-11-14 11:09:37 +0100

    Increase tests

--- test/unit/test_rest.php
+++ test/unit/test_rest.php
@@ -5,7 +5,7 @@ use PHPUnit\Framework\TestCase;
 final class RESTTest extends TestCase
 {
 
-    public function testREST()
+    public function testGetInfrastructureList()
     {
         $_SESSION = array("user"=>"admin", "password"=>"admin");
 
@@ -19,5 +19,53 @@ final class RESTTest extends TestCase
 
         $res = $im->GetInfrastructureList();
         $this->assertEquals(array("infid1","infid2"), $res);
+    }
+
+    public function testGetInfrastructureInfo()
+    {
+        $_SESSION = array("user"=>"admin", "password"=>"admin");
+
+        $resp = new Http_response(200, "vmid1\nvmid2");
+
+        $im = $this->getMockBuilder(IMRest::class)
+            ->setMethods(['BasicRESTCall'])
+            ->getMock();
+        $im->method('BasicRESTCall')
+            ->willReturn($resp);
+
+        $res = $im->GetInfrastructureInfo("infid1");
+        $this->assertEquals(array("vmid1","vmid2"), $res);
+    }
+
+    public function testGetInfrastructureState()
+    {
+        $_SESSION = array("user"=>"admin", "password"=>"admin");
+
+        $resp = new Http_response(200, '{"state":{"state": "running", "vm_states" : {"vmid1": "running", "vmid2": "running"}}}');
+
+        $im = $this->getMockBuilder(IMRest::class)
+            ->setMethods(['BasicRESTCall'])
+            ->getMock();
+        $im->method('BasicRESTCall')
+            ->willReturn($resp);
+
+        $res = $im->GetInfrastructureState("infid1");
+        $this->assertEquals("running", $res);
+    }
+
+    public function testDestroyInfrastructure()
+    {
+        $_SESSION = array("user"=>"admin", "password"=>"admin");
+
+        $resp = new Http_response(200, '');
+
+        $im = $this->getMockBuilder(IMRest::class)
+            ->setMethods(['BasicRESTCall'])
+            ->getMock();
+        $im->method('BasicRESTCall')
+            ->willReturn($resp);
+
+        $res = $im->DestroyInfrastructure("infid1");
+        $this->assertEquals("", $res);
     }
 }
