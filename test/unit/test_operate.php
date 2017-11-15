e4c0a8625ab8388069f535e46c713f227a8a111e
micafer <micafer1@upv.es>
2017-11-15 12:41:26 +0100

    Improve tests

--- test/unit/test_operate.php
+++ test/unit/test_operate.php
@@ -24,8 +24,17 @@ final class OperateTest extends TestCase
         $_SESSION = array("user"=>"admin", "password"=>"admin");
         $_GET = array("op"=>"create");
         $_POST = array("radl"=>"radl");
+
+        $im = $this->getMockBuilder(IMRest::class)
+            ->setMethods(['CreateInfrastructure'])
+            ->getMock();
+        $im->method('CreateInfrastructure')
+            ->willReturn("infid");
+
+        $GLOBALS['mock_im'] = $im;
         include('../../operate.php');
-        $this->assertEquals(array('Location: error.php?msg=Error%3A+Connect+error%3A+Connection+refused+%28111%29'),xdebug_get_headers());
+        unset($GLOBALS['mock_im']);
+        $this->assertEquals(array('Location: list.php'),xdebug_get_headers());
     }
 
     /**
@@ -36,8 +45,17 @@ final class OperateTest extends TestCase
         $this->expectOutputString('');
         $_SESSION = array("user"=>"admin", "password"=>"admin");
         $_GET = array("op"=>"destroy", "id"=>"id");
+
+        $im = $this->getMockBuilder(IMRest::class)
+            ->setMethods(['DestroyInfrastructure'])
+            ->getMock();
+        $im->method('DestroyInfrastructure')
+            ->willReturn("infid");
+
+        $GLOBALS['mock_im'] = $im;
         include('../../operate.php');
-        $this->assertEquals(array('Location: error.php?msg=Error%3A+Connect+error%3A+Connection+refused+%28111%29'),xdebug_get_headers());
+        unset($GLOBALS['mock_im']);
+        $this->assertEquals(array('Location: list.php'),xdebug_get_headers());
     }
 
     /**
@@ -48,8 +66,17 @@ final class OperateTest extends TestCase
         $this->expectOutputString('');
         $_SESSION = array("user"=>"admin", "password"=>"admin");
         $_GET = array("op"=>"destroyvm", "infid"=>"id", "vmid"=>"vid");
+
+        $im = $this->getMockBuilder(IMRest::class)
+            ->setMethods(['RemoveResource'])
+            ->getMock();
+        $im->method('RemoveResource')
+            ->willReturn("1");
+
+        $GLOBALS['mock_im'] = $im;
         include('../../operate.php');
-        $this->assertEquals(array('Location: error.php?msg=Error%3A+Connect+error%3A+Connection+refused+%28111%29'),xdebug_get_headers());
+        unset($GLOBALS['mock_im']);
+        $this->assertEquals(array('Location: list.php'),xdebug_get_headers());
     }
 
     /**
@@ -60,8 +87,17 @@ final class OperateTest extends TestCase
         $this->expectOutputString('');
         $_SESSION = array("user"=>"admin", "password"=>"admin");
         $_GET = array("op"=>"stopvm", "infid"=>"id", "vmid"=>"vid");
+
+        $im = $this->getMockBuilder(IMRest::class)
+            ->setMethods(['StopVM'])
+            ->getMock();
+        $im->method('StopVM')
+            ->willReturn("");
+
+        $GLOBALS['mock_im'] = $im;
         include('../../operate.php');
-        $this->assertEquals(array('Location: error.php?msg=Error%3A+Connect+error%3A+Connection+refused+%28111%29'),xdebug_get_headers());
+        unset($GLOBALS['mock_im']);
+        $this->assertEquals(array('Location: getvminfo.php?id=id&vmid=vid'),xdebug_get_headers());
     }
 
     /**
@@ -72,8 +108,17 @@ final class OperateTest extends TestCase
         $this->expectOutputString('');
         $_SESSION = array("user"=>"admin", "password"=>"admin");
         $_GET = array("op"=>"startvm", "infid"=>"id", "vmid"=>"vid");
+
+        $im = $this->getMockBuilder(IMRest::class)
+            ->setMethods(['StartVM'])
+            ->getMock();
+        $im->method('StartVM')
+            ->willReturn("");
+
+        $GLOBALS['mock_im'] = $im;
         include('../../operate.php');
-        $this->assertEquals(array('Location: error.php?msg=Error%3A+Connect+error%3A+Connection+refused+%28111%29'),xdebug_get_headers());
+        unset($GLOBALS['mock_im']);
+        $this->assertEquals(array('Location: getvminfo.php?id=id&vmid=vid'),xdebug_get_headers());
     }
 
     /**
@@ -85,8 +130,17 @@ final class OperateTest extends TestCase
         $_SESSION = array("user"=>"admin", "password"=>"admin");
         $_GET = array("op"=>"addresource");
         $_POST = array("radl"=>"radl", "infid"=>"id");
+
+        $im = $this->getMockBuilder(IMRest::class)
+            ->setMethods(['AddResource'])
+            ->getMock();
+        $im->method('AddResource')
+            ->willReturn("vmid");
+
+        $GLOBALS['mock_im'] = $im;
         include('../../operate.php');
-        $this->assertEquals(array('Location: error.php?msg=Error%3A+Connect+error%3A+Connection+refused+%28111%29'),xdebug_get_headers());
+        unset($GLOBALS['mock_im']);
+        $this->assertEquals(array('Location: list.php'),xdebug_get_headers());
     }
 
     /**
@@ -97,8 +151,17 @@ final class OperateTest extends TestCase
         $this->expectOutputString('');
         $_SESSION = array("user"=>"admin", "password"=>"admin");
         $_GET = array("op"=>"reconfigure", "infid"=>"id");
+
+        $im = $this->getMockBuilder(IMRest::class)
+            ->setMethods(['Reconfigure'])
+            ->getMock();
+        $im->method('Reconfigure')
+            ->willReturn("");
+
+        $GLOBALS['mock_im'] = $im;
         include('../../operate.php');
-        $this->assertEquals(array('Location: error.php?msg=Error%3A+Connect+error%3A+Connection+refused+%28111%29'),xdebug_get_headers());
+        unset($GLOBALS['mock_im']);
+        $this->assertEquals(array('Location: list.php'),xdebug_get_headers());
     }
 }
 ?>
