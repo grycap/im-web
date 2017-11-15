5bc0061ab4c15979d7b338f1d9fc48b2400fc8d4
micafer <micafer1@upv.es>
2017-11-15 09:38:04 +0100

    Increase tests

---
+++ test/unit/test_userinfo.php
@@ -0,0 +1,126 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class UserinfoTest extends TestCase
+{
+    /**
+     * @runInSeparateProcess
+     */
+    public function testNoOp()
+    {
+        $this->expectOutputString('');
+        $_SESSION = array("user"=>"admin", "password"=>"admin");
+        include('../../userinfo.php');
+        $this->assertEquals(array('Location: error.php?msg=No op'),xdebug_get_headers());
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testCreateIncorrectPass()
+    {
+        $this->expectOutputString('');
+        $_SESSION = array("user"=>"admin", "password"=>"admin");
+        $_GET = array("op"=>"add");
+        $_POST = array("username"=>"userinfotest", "password"=>"passwordtest",
+                    "password2"=>"password", "user_groups"=>"users", "permissions"=>"0");
+        include('../../userinfo.php');
+        $this->assertEquals(array('Location: error.php?msg=The+passwords+are+not+equal.'),xdebug_get_headers());
+
+        $res = get_user("userinfotest");
+        $this->assertEquals(NULL, $res);
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testCreate()
+    {
+        $this->expectOutputString('');
+        $_SESSION = array("user"=>"admin", "password"=>"admin");
+        $_GET = array("op"=>"add");
+        $_POST = array("username"=>"userinfotest", "password"=>"passwordtest",
+                    "password2"=>"passwordtest", "user_groups"=>"users", "permissions"=>"0");
+        include('../../userinfo.php');
+        $this->assertEquals(array('Location: user_list.php'),xdebug_get_headers());
+
+        $res = get_user("userinfotest");
+        $this->assertEquals("0", $res['permissions']);
+    }
+
+    /**
+     * @runInSeparateProcess
+     * @depends testCreate
+     */
+    public function testEdit()
+    {
+        $this->expectOutputString('');
+
+        $_SESSION = array("user"=>"admin", "password"=>"admin");
+        $_GET = array("op"=>"edit");
+        $_POST = array("id"=>"userinfotest", "username"=>"newuserinfotest", "password"=>"passwordtest",
+                    "password2"=>"passwordtest", "user_groups"=>"users", "permissions"=>"0");
+        include('../../userinfo.php');
+        $this->assertEquals(array('Location: user_list.php'),xdebug_get_headers());
+
+        $res = get_user("newuserinfotest");
+        $this->assertEquals("0", $res['permissions']);
+    }
+
+    /**
+     * @runInSeparateProcess
+     * @depends testCreate
+     */
+    public function testChangePassword()
+    {
+        $this->expectOutputString('');
+
+        $_SESSION = array("user"=>"newuserinfotest", "password"=>"passwordtest");
+        $_GET = array("op"=>"password");
+        $_POST = array("password"=>"npasswordtest", "password2"=>"npasswordtest");
+        include('../../userinfo.php');
+        $this->assertEquals(array('Location: list.php'),xdebug_get_headers());
+
+        $res = get_user("newuserinfotest");
+        $res = check_password("npasswordtest", $res["password"]);
+        $this->assertEquals(true, $res);
+    }
+
+    /**
+     * @runInSeparateProcess
+     * @depends testCreate
+     */
+    public function testRegister()
+    {
+        $this->expectOutputString('');
+
+        $_SESSION = array("user"=>"newuserinfotest", "password"=>"passwordtest");
+        $_GET = array("op"=>"register");
+        $_POST = array("username"=>"userinfotest2", "password"=>"npasswordtest", "password2"=>"npasswordtest");
+        include('../../userinfo.php');
+        $this->assertEquals(array('Location: index.php?info=User added successfully'),xdebug_get_headers());
+
+        $res = get_user("userinfotest2");
+        $res = check_password("npasswordtest", $res["password"]);
+        $this->assertEquals(true, $res);
+
+        $err = delete_user("userinfotest2");
+        $this->assertEquals("", $err);
+    }
+
+    /**
+     * @runInSeparateProcess
+     * @depends testCreate
+     */
+    public function testDelete()
+    {
+        $this->expectOutputString('');
+
+        $_SESSION = array("user"=>"admin", "password"=>"admin");
+        $_GET = array("op"=>"delete", "id"=>"newuserinfotest");
+        include('../../userinfo.php');
+        $this->assertEquals(array('Location: user_list.php'),xdebug_get_headers());
+    }
+}
+?>
