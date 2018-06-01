5b1e007461cffbf6a298798c2d4fdcd6f85bd172
micafer <micafer1@upv.es>
2018-06-01 10:09:15 +0200

    Fix: #66

--- user_connected.php
+++ user_connected.php
@@ -1,18 +1,18 @@
 <?php
 if (isset($_SESSION['user_name'])) {
-        $user = $_SESSION['user_name'];
+        $user_name = $_SESSION['user_name'];
 } elseif (isset($_SESSION['user'])) {
-        $user = $_SESSION['user'];
+		$user_name = $_SESSION['user'];
 }
 ?>
 <div id="caja_nombre_usuario">
         <div id="texto_open_sans">
                 <?php 
-                        if (isset($user)) {
+                	if (isset($user_name)) {
                 ?>
                 &nbsp&nbsp&nbsp&nbsp<img style="vertical-align: middle;" src="images/verde.png" />
                 <?php 
-                                echo $user;
+                                echo $user_name;
                                 echo ' is connected'; 
                 ?> 
 &nbsp&nbsp|&nbsp&nbsp <a href = "logout.php">Sign out</a>
